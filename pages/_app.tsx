e5a8b469e9f25d95a208cdd180d75267a99e1dce
QueGonJinn <molialow13@gmail.com>
2022-08-19 16:37:51 +0300

    feat: creating global CSS styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+	return <>
+		<Head>
+			<title>MyTop</title>
+			<link rel="icon" href='/public/favicon.ico' />
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			
+		</Head>
+		<Component {...pageProps} />
+	</>;
 }
 
 export default MyApp;
