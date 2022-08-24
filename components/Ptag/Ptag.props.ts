10b0facf3cccb6d72668980855c7f0a247e15e16
QueGonJinn <molialow13@gmail.com>
2022-08-24 14:00:00 +0300

    feat: tag component implementation

--- components/Ptag/Ptag.props.ts
+++ components/Ptag/Ptag.props.ts
@@ -1,6 +1,6 @@
-import { ReactNode } from 'react';
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
 
-export interface PtagProps {
+export interface PtagProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
 	children: ReactNode;
-	size: 'm' | 'l' | 's';
+	size?: 'm' | 'l' | 's';
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Htag, Button, Ptag } from '../components';
+import { Htag, Button, Ptag, Tag } from '../components';
 
 export default function Home(): JSX.Element {
 	return (
@@ -9,9 +9,14 @@ export default function Home(): JSX.Element {
 			<Htag tag='h2'>Text2</Htag>
 			<Button appearance='primary'>Кнопка</Button>
 			<Button appearance='ghost' arrow='down'>Кнопка</Button>
-			<Ptag size='m'>dfgdfgd</Ptag>
+			<Ptag >dfgdfgd</Ptag>
 			<Ptag size='l'>dfgdfgd</Ptag>
 			<Ptag size='s'>dfgdfgd</Ptag>
+			<Tag color='ghost'>Hello</Tag>
+			<Tag color='red'>Hello</Tag>
+			<Tag color='gray'>Hello</Tag>
+			<Tag color='green'>Hello</Tag>
+			<Tag color='primary' href='#'>Hello</Tag>
 		</>
 	);
 }
